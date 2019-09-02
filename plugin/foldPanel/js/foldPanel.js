f3c0aebf5ad244f8037cfbec0636749393edc061
lz <1678881243@qq.com>
2019-09-02 17:45:07 +0800

    模板

--- plugin/foldPanel/js/foldPanel.js
+++ plugin/foldPanel/js/foldPanel.js
@@ -5,21 +5,39 @@
     $.fn.foldPanel = function() {
         var that = $(this);
         $(".subNav").click(function() {
-            that.find('i').removeClass("fa-chevron-down")
-                .addClass('fa-chevron-right');
-            $(this).toggleClass("currentDd")
-                .siblings(".subNav")
-                .removeClass("currentDd")
-            $(this).toggleClass("currentDt")
-                .siblings(".subNav")
-                .removeClass("currentDt")
-            $(this).find("i")
-                .removeClass("fa-chevron-right")
-                .addClass('fa-chevron-down');
-            $(this).next(".navContent")
-                .slideToggle(300)
-                .siblings(".navContent")
-                .slideUp(500)
+            console.log($(this).attr('class'))
+            if ($(this).attr('class') == 'subNav') {
+                that.find('i')
+                    .removeClass("fa-chevron-down")
+                    .addClass('fa-chevron-right');
+                $(this).toggleClass("currentDd")
+                    .siblings(".subNav")
+                    .removeClass("currentDd")
+                $(this).toggleClass("currentDt")
+                    .siblings(".subNav")
+                    .removeClass("currentDt")
+                $(this).find("i")
+                    .removeClass("fa-chevron-right")
+                    .addClass('fa-chevron-down');
+                $(this).next(".navContent")
+                    .slideToggle(300)
+                    .siblings(".navContent")
+                    .slideUp(500)
+            } else {
+                $(this).toggleClass("currentDd")
+                    .siblings(".subNav")
+                    .removeClass("currentDd")
+                $(this).toggleClass("currentDt")
+                    .siblings(".subNav")
+                    .removeClass("currentDt")
+                $(this).find("i")
+                    .removeClass("fa-chevron-down")
+                    .addClass('fa-chevron-right');
+                $(this).next(".navContent")
+                    .slideToggle(300)
+                    .siblings(".navContent")
+                    .slideUp(500)
+            }
         })
     }
 })(jQuery)
