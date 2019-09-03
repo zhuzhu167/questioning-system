ef329bd8070614e8533e41d0fdd7b58cf502babf
lz <1678881243@qq.com>
2019-09-03 09:50:52 +0800

    补充使用说明

---
+++ views/index/index.js
@@ -0,0 +1,7 @@
+// 数据模型
+// 利用 ajax 获取数据到此处存放，之后到视图层更新页面数据
+// 此处只管数据操作，执行完 return 数据，不要有其他视图操作，增加代码可读性
+var url = ["../home/home.html ", "../appointment/appointment.html ", "../medicalAd/medicalAd.html ",
+    "../registration/registration.html ", "../retail/retail.html ", "../report/report.html ",
+    "../set/set.html ", "../charge/charge.html", "../medicine/medicine.html"
+]
